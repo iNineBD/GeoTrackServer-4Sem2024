d6d241a8091b3a690b8b01d0ed34f05fb7ad3248
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-08-31 21:03:36 -0300

    fix: .gitignore

---
+++ api-geotrack/src/main/java/api_geotrack/com/api_geotrack/GeotrackApplication.java
@@ -0,0 +1,13 @@
+package api_geotrack.com.api_geotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeotrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeotrackApplication.class, args);
+	}
+
+}
