01dcd420b2c13f87bf8983a2dd12c1b85480383a
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-10-07 02:26:12 -0300

    feature: controller with endpoint to insert geometry zone

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/controllers/GeometryController.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/controllers/GeometryController.java
@@ -1,7 +1,9 @@
 package com.geotrack.apigeotrack.controllers;
 
+import com.geotrack.apigeotrack.dto.geometry.GeometryZoneRequestDTO;
 import com.geotrack.apigeotrack.service.GeometryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/session")
+@RequestMapping("/zone")
 public class GeometryController {
 
+    @Autowired
+    GeometryService geometryService;
+
+    @PostMapping("/add")
+    public ResponseEntity<String> insertPolygon(@RequestBody GeometryZoneRequestDTO geometryZoneRequestDTO) {
+        geometryService.insertGeometryZone(geometryZoneRequestDTO);  // Chama o metodo de inserir a zona
+        return new ResponseEntity<>("Zone inserted successfully", HttpStatus.CREATED);
+    }
 
 }
