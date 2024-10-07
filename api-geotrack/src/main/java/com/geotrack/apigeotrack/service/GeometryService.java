31427f32db80981b201a68cc1279b6b8a225dfc8
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-10-07 20:18:46 -0300

    fix: save unnecessary, implements coordinates validator

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/service/GeometryService.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/service/GeometryService.java
@@ -1,15 +1,14 @@
 package com.geotrack.apigeotrack.service;
 
 import com.geotrack.apigeotrack.dto.geometry.GeometryZoneRequestDTO;
-import com.geotrack.apigeotrack.dto.geometry.ZoneCoordenatesDTO;
 import com.geotrack.apigeotrack.entities.GeometryCoordinates;
 import com.geotrack.apigeotrack.entities.GeometryZone;
 import com.geotrack.apigeotrack.repositories.GeometryCoordinatesRepository;
 import com.geotrack.apigeotrack.repositories.GeometryZoneRepository;
+import com.geotrack.apigeotrack.service.utils.CoordinatesValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,40 +28,18 @@ public class GeometryService {
         if (geometryZoneRequestDTO.name().isEmpty()) {
             throw new IllegalArgumentException("Defina um nome para sua Zona Geometrica!");
         }
-        // verificação das coordenadas
-        for (ZoneCoordenatesDTO coordinates : geometryZoneRequestDTO.coordinates()) {
-
-            // verifica se long e lati não estão vazios
-            if (coordinates.longitude() == null) {
-                throw new IllegalArgumentException("Longitude não pode ser nula.");
-            }
-            if (coordinates.latitude() == null) {
-                throw new IllegalArgumentException("Latitude não pode ser nula.");
-            }
-            // verifica as coordenadas (longitude e latitude)
-            if (coordinates.longitude().compareTo(BigDecimal.valueOf(-180)) < 0 ||
-                    coordinates.longitude().compareTo(BigDecimal.valueOf(180)) > 0) {
-                throw new IllegalArgumentException("Longitude inválida: " + coordinates.longitude() + ". Deve estar " +
-                        "entre -180 e 180 graus.");
-            }
-            if (coordinates.latitude().compareTo(BigDecimal.valueOf(-90)) < 0 ||
-                    coordinates.latitude().compareTo(BigDecimal.valueOf(90)) > 0) {
-                throw new IllegalArgumentException("Latitude inválida: " + coordinates.latitude() + ". Deve estar " +
-                        "entre -90 e 90 graus.");
-            }
-        }
 
         // cria objeto para envio
         GeometryZone geometryZone = new GeometryZone();
         geometryZone.setNome(geometryZoneRequestDTO.name());
         geometryZone.setType(geometryZoneRequestDTO.type());
 
-        // salva GeometryZone primeiro para gerar o id
-        geometryZoneRepository.save(geometryZone);
-
         // Agora, cria as entidades GeometryCoordinates com base nos dados do requestDTO
         List<GeometryCoordinates> coordinatesEntities = geometryZoneRequestDTO.coordinates().stream()
                 .map(coordDTO -> {
+
+                    CoordinatesValidator.validatesCoordinator(coordDTO.longitude(), coordDTO.latitude());
+
                     GeometryCoordinates geometryCoordinates = new GeometryCoordinates();
                     geometryCoordinates.setLongitude(coordDTO.longitude());
                     geometryCoordinates.setLatitude(coordDTO.latitude());
@@ -75,4 +52,5 @@ public class GeometryService {
         // salva a GeometryZone com suas coordenadas
         geometryZoneRepository.save(geometryZone);
     }
+
 }
