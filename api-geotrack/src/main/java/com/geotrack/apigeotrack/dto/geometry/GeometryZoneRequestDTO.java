d608217bfbcb7916b66bf04146fbd38abc82ab7e
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-10-07 02:22:34 -0300

    fix: type in request

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/geometry/GeometryZoneRequestDTO.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/geometry/GeometryZoneRequestDTO.java
@@ -2,11 +2,14 @@ package com.geotrack.apigeotrack.dto.geometry;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.geotrack.apigeotrack.service.utils.GeometryForms;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record GeometryZoneRequestDTO(
-        @JsonAlias("name") String name,
-        @JsonAlias("coordinates") List<ZoneCoordenatesDTO> coordinates) {
+        @JsonAlias("name") String name, // nome da zona
+        @JsonAlias("type") GeometryForms type, // enum das formas geometricas
+        @JsonAlias("coordinates") List<ZoneCoordenatesDTO> coordinates) // lista das coordenadas
+{
 }
