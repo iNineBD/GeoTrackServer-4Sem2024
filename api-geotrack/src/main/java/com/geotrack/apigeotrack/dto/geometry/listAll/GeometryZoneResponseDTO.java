5f83249fff1ad9afa400a143dc12e8e38c57ad93
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-10-09 01:09:54 -0300

    feat: dto to return list with all geometry zones

---
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/geometry/listAll/GeometryZoneResponseDTO.java
@@ -0,0 +1,16 @@
+package com.geotrack.apigeotrack.dto.geometry.listAll;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.geotrack.apigeotrack.dto.geometry.insert.CenterCoordinatesDTO;
+import com.geotrack.apigeotrack.service.utils.GeometryForms;
+
+import java.math.BigDecimal;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record GeometryZoneResponseDTO(@JsonAlias("id") Integer id,
+                                      @JsonAlias("name") String name,
+                                      @JsonAlias("type") GeometryForms type,
+                                      @JsonAlias("center") CenterCoordinatesDTO center,
+                                      @JsonAlias("radius") BigDecimal radius) {
+}
