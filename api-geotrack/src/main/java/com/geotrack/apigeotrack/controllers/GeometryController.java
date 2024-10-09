41b7e98107710f3db10f9cf6cf2bcc87a955232e
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-10-08 22:42:35 -0300

    feat: endpoint to insert and list all zones

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/controllers/GeometryController.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/controllers/GeometryController.java
@@ -1,15 +1,15 @@
 package com.geotrack.apigeotrack.controllers;
 
-import com.geotrack.apigeotrack.dto.geometry.GeometryZoneRequestDTO;
+import com.geotrack.apigeotrack.dto.geometry.insert.GeometryZoneRequestDTO;
+import com.geotrack.apigeotrack.dto.geometry.GeometryZoneResponseDTO;
 import com.geotrack.apigeotrack.service.GeometryService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Geometry Zones", description = "Operations to geometry zones to insert, listing and exclusion")
 @RestController
@@ -21,9 +21,19 @@ public class GeometryController {
 
     // method to insert geometry zones
     @PostMapping("/add")
-    public ResponseEntity<String> insertPolygon(@RequestBody GeometryZoneRequestDTO geometryZoneRequestDTO) {
-
+    public ResponseEntity<String> insertGeometryZones(@RequestBody GeometryZoneRequestDTO geometryZoneRequestDTO) {
+        geometryService.insertGeometryZones(geometryZoneRequestDTO);
         return new ResponseEntity<>("Zone inserted successfully", HttpStatus.CREATED);
     }
+
+    @GetMapping("list")
+    public ResponseEntity<List<GeometryZoneResponseDTO>> getAllZones(){
+        geometryService.getAll();
+        return ResponseEntity.ok().body(geometryService.getAll());
+    }
+
+
+
+
 
 }
