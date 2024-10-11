8ebf13a4c7765323f3ccf40b46a2247b11bd2726
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-10-10 21:01:48 -0300

    fix: fix method names and comments

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/controllers/GeometryController.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/controllers/GeometryController.java
@@ -1,9 +1,9 @@
 package com.geotrack.apigeotrack.controllers;
 
-import com.geotrack.apigeotrack.dto.zone.delete.DeleteZoneDTO;
-import com.geotrack.apigeotrack.dto.zone.update.UpdateGeometryZonesDTO;
-import com.geotrack.apigeotrack.dto.zone.insert.GeometryZoneRequestDTO;
-import com.geotrack.apigeotrack.dto.zone.list.GeometryZoneResponseDTO;
+import com.geotrack.apigeotrack.dto.zone.deleteCircle.DeleteZoneDTO;
+import com.geotrack.apigeotrack.dto.zone.updateCircle.UpdateGeometryZonesDTO;
+import com.geotrack.apigeotrack.dto.zone.insertCircle.GeometryZoneRequestDTO;
+import com.geotrack.apigeotrack.dto.zone.getAllCircle.GeometryZoneResponseDTO;
 import com.geotrack.apigeotrack.service.GeometryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "Geometry Zones", description = "Operations to zone zones to insert, listing and exclusion")
+@Tag(name = "Geometry Zones", description = "Operations to zone zones to insertCircle, listing and exclusion")
 @RestController
 @RequestMapping("/zone")
 public class GeometryController {
@@ -22,7 +22,7 @@ public class GeometryController {
     @Autowired
     GeometryService geometryService;
 
-    // method to insert zone zones
+    // method to insertCircle zone zones
     @Operation(summary = "Adiciona uma Zona Geométrica ao Banco de dados")
     @PostMapping("/insert")
     public ResponseEntity<String> insertCircle(@RequestBody GeometryZoneRequestDTO geometryZoneRequestDTO) {
@@ -32,21 +32,21 @@ public class GeometryController {
 
     @Operation(summary = "Listagem de Zonas Geométricas", description = "Retorna uma lista de Zonas Geométricas ativas no banco de dados")
     @GetMapping("/list")
-    public ResponseEntity<List<GeometryZoneResponseDTO>> getAllZones() {
+    public ResponseEntity<List<GeometryZoneResponseDTO>> getAllCircle() {
         geometryService.listAllGeometryZones();
         return ResponseEntity.ok().body(geometryService.listAllGeometryZones());
     }
 
     @Operation(summary = "Deleta uma Zona Geométrica", description = "Deleta uma Zona Geométrica do banco de dados, alterando o status para 0")
     @PostMapping("/delete")
-    public ResponseEntity<String> deleteGeometryZones(@RequestBody DeleteZoneDTO deleteZoneDTO) {
+    public ResponseEntity<String> deleteCircle(@RequestBody DeleteZoneDTO deleteZoneDTO) {
         geometryService.deleteZones(deleteZoneDTO);
         return new ResponseEntity<>("Zona deletada com Sucesso", HttpStatus.OK);
     }
 
     @Operation(summary = "Edita uma Zona Geométrica", description = "Recebe os dados alterados de uma zona geométrica, e salva-os no banco de dados")
     @PutMapping("/update")
-    public ResponseEntity<String> updateGeometryZones(@RequestBody UpdateGeometryZonesDTO updateGeometryZonesDTO){
+    public ResponseEntity<String> updateCircle(@RequestBody UpdateGeometryZonesDTO updateGeometryZonesDTO){
         geometryService.editZones(updateGeometryZonesDTO);
         return new ResponseEntity<>("Zona Geométrica editada com Sucesso",HttpStatus.OK);
     }
