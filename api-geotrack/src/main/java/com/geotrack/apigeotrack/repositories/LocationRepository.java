667851bddcf2f2f9769ae9149c951733d9ac5c2e
Anaraquely <anaraquelysmachado29@gmail.com>
2024-10-10 22:27:14 -0300

    feat: adding findOneLocalizationGroupedByDateWithInterval search

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/repositories/LocationRepository.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/repositories/LocationRepository.java
@@ -38,6 +38,30 @@ public interface LocationRepository extends JpaRepository<Location, Integer> {
             "  ORDER BY\n" +
             "    grouped_data.id_dispositivo,time_group", nativeQuery = true)
     List<Object[]> findLocalizationGroupedByDateWithInterval(List<Long> idsDev, LocalDate startDate, LocalDate finalDate);
-}
 
+    @Query(value = "WITH grouped_data AS (\n" +
+            "    SELECT \n" +
+            "        FLOOR((CAST(l.data_hora AS DATE) - TO_DATE('1970-01-01', 'YYYY-MM-DD')) * 24 * 60 / 15) AS time_group,\n" +
+            "        l.latitude,\n" +
+            "        l.longitude,\n" +
+            "        l.data_hora,\n" +
+            "        COUNT(*) OVER (PARTITION BY FLOOR((CAST(l.data_hora AS DATE) - TO_DATE('1970-01-01', 'YYYY-MM-DD')) * 24 * 60 / 15)) AS count\n" +
+            "    FROM ito1.localizacao l \n" +
+            "    WHERE l.id_dispositivo = :idDev \n" +
+            "      AND l.data_referencia >= :startDate AND l.data_referencia <= :finalDate\n" +
+            ")\n" +
+            "SELECT \n" +
+            "    time_group,\n" +
+            "    AVG(latitude) AS avg_latitude, \n" +
+            "    AVG(longitude) AS avg_longitude,\n" +
+            "    COUNT(*) AS contador,\n" +
+            "    MIN(data_hora) AS start_time,\n" +
+            "    MAX(data_hora) AS end_time,\n" +
+            "    LISTAGG(latitude || ';' || longitude, '|') WITHIN GROUP (ORDER BY latitude, longitude) AS lat_long_list\n" +
+            "FROM grouped_data\n" +
+            "GROUP BY time_group\n" +
+            "HAVING count(*) > 2\n" +
+            "ORDER BY time_group", nativeQuery = true)
+    List<Object[]> findOneLocalizationGroupedByDateWithInterval(Long idDev, LocalDate startDate, LocalDate finalDate);
+}
 
