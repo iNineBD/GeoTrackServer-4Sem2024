a1e50466b6e4aad9984d23637400e377e86d9356
Anaraquely <anaraquelysmachado29@gmail.com>
2024-10-11 19:23:53 -0300

    Doc: adding tag in StopPointSessionController to swagger

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/controllers/StopPointSessionController.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/controllers/StopPointSessionController.java
@@ -3,11 +3,12 @@ package com.geotrack.apigeotrack.controllers;
 import com.geotrack.apigeotrack.dto.stopointsessions.StopPointSessionRequestDTO;
 import com.geotrack.apigeotrack.dto.stopointsessions.StopPointSessionResponseDTO;
 import com.geotrack.apigeotrack.service.StopPointSessionService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
+@Tag(name = "Stop Points in Session", description = "Operations to return stopping points of devices in Geographics Sessions")
 @RestController
 @RequestMapping("/stoppointsession")
 public class StopPointSessionController {
