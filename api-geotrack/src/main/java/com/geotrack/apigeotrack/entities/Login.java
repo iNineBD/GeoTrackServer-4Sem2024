1b1b80ad3fc7ee14b858db61cd5d6af880db3918
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-10-24 22:14:52 -0300

    fix: entity login with user details

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/entities/Login.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/entities/Login.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Table(name = "login", schema = "ito1")
@@ -12,20 +17,56 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class Login {
+public class Login implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
-    @Column(name = "nome",length = 200)
+    @Column(name = "nome", length = 200)
     private String name;
 
-    @Column(name = "senha",length = 200)
+    @Column(name = "senha", length = 200)
     private String password;
 
-    @Column(name = "email",length = 200, unique = true)
+    @Column(name = "email", length = 200, unique = true)
     private String email;
 
+    // method UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Return a empty list because we are not using roles
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getUsername() {
+        // using email as username
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // returning true because we are not controlling account expiration
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // returning true because we are not controlling account lock
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // returning true because we are not controlling credentials expiration
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // returning true because we are not controlling account enablement
+        return true;
+    }
 }
