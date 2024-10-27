c9f5ae7597a5bbae180d4b106af7c4bb5ea16d67
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-10-26 22:20:47 -0300

    style: indexation

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/login/RegisterRequestDTO.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/login/RegisterRequestDTO.java
@@ -5,5 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record RegisterRequestDTO(@NotNull @JsonAlias("name") String name, @NotNull @JsonAlias("email") String email, @NotNull @JsonAlias("password") String password) {
+public record RegisterRequestDTO(@NotNull @JsonAlias("name") String name,
+                                 @NotNull @JsonAlias("email") String email,
+                                 @NotNull @JsonAlias("password") String password) {
 }
