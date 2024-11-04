da1dc7e90279c2e5e01d9ec5e3eb2561a57eabcd
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-11-04 20:38:03 -0300

    feat: filter users swagger schema

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/filterusers/RequestUser.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/filterusers/RequestUser.java
@@ -2,7 +2,14 @@ package com.geotrack.apigeotrack.dto.filterusers;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record RequestUser(@JsonAlias("page") int page, @JsonAlias("page") int qtdPage)  {
+public record RequestUser(@Schema(description = "Número da página para a paginação", example = "1")
+                          @JsonAlias("page")
+                          int page,
+
+                          @Schema(description = "Quantidade de itens por página", example = "10")
+                          @JsonAlias("qtdPage")
+                          int qtdPage)  {
 }
--- api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/filterusers/ResponseUsers.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/dto/filterusers/ResponseUsers.java
@@ -2,11 +2,20 @@ package com.geotrack.apigeotrack.dto.filterusers;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ResponseUsers(@JsonAlias("UsersList") List<DataUsersDTO> listUsers,
-                            @JsonAlias("currentPage") int currentePage,
-                            @JsonAlias("totalPages") int totalPages) {
+public record ResponseUsers(@Schema(description = "Lista de usuários disponíveis para a página atual")
+                            @JsonAlias("UsersList")
+                            List<DataUsersDTO> listUsers,
+
+                            @Schema(description = "Página atual da paginação", example = "1")
+                            @JsonAlias("currentPage")
+                            int currentPage,
+
+                            @Schema(description = "Total de páginas disponíveis", example = "5")
+                            @JsonAlias("totalPages")
+                            int totalPages) {
 }
