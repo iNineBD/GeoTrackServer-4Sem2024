b17fea14f17780705c1db92e03dd73a69c167102
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2024-11-09 16:08:26 -0300

    chore: change white list

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/configurations/SecurityConfig.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/configurations/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
     SecurityFilter securityFilter;
 
     private static final String[] WHITE_LIST_URL = {
-            "/**",
             "/auth/**",
             "/v2/api-docs",
             "/v3/api-docs",
