709431a1eec4e446de0a5439119fc016903b4e5a
Anaraquely <anaraquelysmachado29@gmail.com>
2024-11-26 20:08:28 -0300

    Refactor: stop points query

--- api-geotrack/src/main/java/com/geotrack/apigeotrack/repositories/LocationRepository.java
+++ api-geotrack/src/main/java/com/geotrack/apigeotrack/repositories/LocationRepository.java
@@ -14,93 +14,73 @@ public interface LocationRepository extends JpaRepository<Location, Integer> {
     List<Object[]> findStopPointsByUsers(List<Long> idsDev, LocalDate startDate, LocalDate finalDate);
 
     @Query(value = "WITH cte_localizacao AS (\n" +
-            "    SELECT \n" +
-            "        ID_LOCALIZACAO,\n" +
-            "        latitude,\n" +
-            "        longitude,\n" +
-            "        DATA_HORA,\n" +
-            "        DATA_REFERENCIA AS DATA_REF,\n" +
-            "        id_dispositivo,\n" +
-            "        LAG(DATA_HORA) OVER (PARTITION BY id_dispositivo ORDER BY DATA_HORA) AS DATA_HORA_ANTERIOR,\n" +
-            "        -- Marca 1 onde a diferença é maior que 15 minutos, caso contrário 0\n" +
-            "        CASE \n" +
-            "            WHEN DATA_HORA - LAG(DATA_HORA) OVER (PARTITION BY id_dispositivo ORDER BY DATA_HORA) > INTERVAL '15' MINUTE \n" +
-            "            THEN 1 \n" +
-            "            ELSE 0 \n" +
-            "        END AS MARCA_GRUPO\n" +
-            "    FROM \n" +
-            "        ito1.localizacao\n" +
-            "    WHERE \n" +
-            "        id_dispositivo IN (:idDev)\n" +
-            "        AND DATA_REFERENCIA BETWEEN TO_DATE(:startDate, 'YYYY-MM-DD') \n" +
-            "                              AND TO_DATE(:finalDate, 'YYYY-MM-DD')\n" +
-            "),\n" +
-            "cte_grupos AS (\n" +
-            "    SELECT \n" +
-            "        ID_LOCALIZACAO,\n" +
-            "        latitude,\n" +
-            "        longitude,\n" +
-            "        DATA_HORA,\n" +
-            "        DATA_REF,\n" +
-            "        id_dispositivo,\n" +
-            "        DATA_HORA_ANTERIOR,\n" +
-            "        MARCA_GRUPO,\n" +
-            "        -- Incrementa o grupo toda vez que encontrar um 1 em MARCA_GRUPO\n" +
-            "        SUM(MARCA_GRUPO) \n" +
-            "        OVER (PARTITION BY id_dispositivo ORDER BY DATA_HORA ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) + 1 AS GRUPO_LOCALIZACAO\n" +
-            "    FROM \n" +
-            "        cte_localizacao\n" +
-            "),\n" +
-            "cte_contagem AS (\n" +
-            "    SELECT \n" +
-            "        GRUPO_LOCALIZACAO,\n" +
-            "        id_dispositivo,\n" +
-            "        COUNT(*) AS TOTAL_ITENS,\n" +
-            "        AVG(latitude) AS MEDIA_LATITUDE,\n" +
-            "        AVG(longitude) AS MEDIA_LONGITUDE\n" +
-            "    FROM \n" +
-            "        cte_grupos\n" +
-            "    GROUP BY \n" +
-            "        GRUPO_LOCALIZACAO, id_dispositivo\n" +
-            ")\n" +
-            "SELECT \n" +
-            "    g.ID_LOCALIZACAO,\n" +
-            "    g.latitude,\n" +
-            "    g.longitude,\n" +
-            "    g.DATA_HORA,\n" +
-            "    g.id_dispositivo,\n" +
-            "    g.grupo_localizacao,\n" +
-            "    c.MEDIA_LATITUDE,\n" +
-            "    c.MEDIA_LONGITUDE\n" +
-            "FROM \n" +
-            "    cte_grupos g\n" +
-            "JOIN \n" +
-            "    cte_contagem c\n" +
-            "ON \n" +
-            "    g.GRUPO_LOCALIZACAO = c.GRUPO_LOCALIZACAO\n" +
-            "   AND g.id_dispositivo = c.id_dispositivo\n" +
-            "   AND c.total_itens >= 15\n" +
-            "ORDER BY \n" +
-            "    g.id_dispositivo, g.DATA_HORA;\n" +
-            "\n" +
-            "\n" +
-            "\n" +
-            "SELECT \n" +
-            "    id_dispositivo,\n" +
-            "    avg_latitude,\n" +
-            "    avg_longitude,\n" +
-            "    contador,\n" +
-            "    grupo_localizacao,\n" +
-            "    latitude,\n" +
-            "    longitude,\n" +
-            "    data_hora\n" +
-            "FROM \n" +
-            "    cte_filtrados\n" +
-            "    \n" +
-            "WHERE \n" +
-            "(end_time - start_time) > INTERVAL '15' MINUTE\n" +
-            "ORDER BY \n" +
-            "    grupo_localizacao, data_hora", nativeQuery = true)
+            "                SELECT \n" +
+            "                    ID_LOCALIZACAO,\n" +
+            "                    latitude,\n" +
+            "                    longitude,\n" +
+            "                    DATA_HORA,\n" +
+            "                    DATA_REFERENCIA AS DATA_REF,\n" +
+            "                    id_dispositivo,\n" +
+            "                    LAG(DATA_HORA) OVER (PARTITION BY id_dispositivo ORDER BY DATA_HORA) AS DATA_HORA_ANTERIOR,\n" +
+            "                    -- Marca 1 onde a diferença é maior que 15 minutos, caso contrário 0\n" +
+            "                    CASE \n" +
+            "                        WHEN DATA_HORA - LAG(DATA_HORA) OVER (PARTITION BY id_dispositivo ORDER BY DATA_HORA) > INTERVAL '15' MINUTE \n" +
+            "                        THEN 1 \n" +
+            "                        ELSE 0 \n" +
+            "                    END AS MARCA_GRUPO\n" +
+            "                FROM \n" +
+            "                    ito1.localizacao\n" +
+            "                WHERE \n" +
+            "                    id_dispositivo IN (:idDev)\n" +
+            "                    AND DATA_REFERENCIA BETWEEN TO_DATE(:startDate, 'YYYY-MM-DD') \n" +
+            "                                          AND TO_DATE(:finalDate, 'YYYY-MM-DD')\n" +
+            "            ),\n" +
+            "            cte_grupos AS (\n" +
+            "                SELECT \n" +
+            "                    ID_LOCALIZACAO,\n" +
+            "                    latitude,\n" +
+            "                    longitude,\n" +
+            "                    DATA_HORA,\n" +
+            "                    DATA_REF,\n" +
+            "                    id_dispositivo,\n" +
+            "                    DATA_HORA_ANTERIOR,\n" +
+            "                    MARCA_GRUPO,\n" +
+            "                    -- Incrementa o grupo toda vez que encontrar um 1 em MARCA_GRUPO\n" +
+            "                    SUM(MARCA_GRUPO) \n" +
+            "                    OVER (PARTITION BY id_dispositivo ORDER BY DATA_HORA ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) + 1 AS GRUPO_LOCALIZACAO\n" +
+            "                FROM \n" +
+            "                    cte_localizacao\n" +
+            "            ),\n" +
+            "            cte_contagem AS (\n" +
+            "                SELECT \n" +
+            "                    GRUPO_LOCALIZACAO,\n" +
+            "                    id_dispositivo,\n" +
+            "                    COUNT(*) AS TOTAL_ITENS,\n" +
+            "                    AVG(latitude) AS MEDIA_LATITUDE,\n" +
+            "                    AVG(longitude) AS MEDIA_LONGITUDE\n" +
+            "                FROM \n" +
+            "                    cte_grupos\n" +
+            "                GROUP BY \n" +
+            "                    GRUPO_LOCALIZACAO, id_dispositivo\n" +
+            "            )\n" +
+            "            SELECT \n" +
+            "                g.id_dispositivo,\n" +
+            "                g.latitude,\n" +
+            "                g.longitude,\n" +
+            "                g.DATA_HORA,\n" +
+            "                g.grupo_localizacao,\n" +
+            "                c.MEDIA_LATITUDE,\n" +
+            "                c.MEDIA_LONGITUDE\n" +
+            "            FROM \n" +
+            "                cte_grupos g\n" +
+            "            JOIN \n" +
+            "                cte_contagem c\n" +
+            "            ON \n" +
+            "                g.GRUPO_LOCALIZACAO = c.GRUPO_LOCALIZACAO\n" +
+            "               AND g.id_dispositivo = c.id_dispositivo\n" +
+            "               AND c.total_itens >= 15\n" +
+            "            ORDER BY \n" +
+            "                g.id_dispositivo, g.DATA_HORA", nativeQuery = true)
     List<Object[]> findStopPointsByUsersAndSession(Long idDev, LocalDate startDate, LocalDate finalDate);
 
 
